2b655d5f01df45dafe5ac271406ce48ebeff00fb
45deg <noreply>
2017-02-06 15:24:53 +0900

    Refined pager

--- src/views/articles.js
+++ src/views/articles.js
@@ -10,7 +10,7 @@ var Articles = {
   view(){
     return <main class="fl w-80 ph2">
     <Pager />
-    <section class={Arxiv.waiting ? "o-50" : ""}>
+    <section class={Arxiv.waiting ? "o-50 pb2" : "pb2"}>
     {
       Arxiv.articles.map(article =>
         <article class="bb b--black-60 pa2">
@@ -37,6 +37,7 @@ var Articles = {
       )
     }
     </section>
+    <Pager />
     </main>
   }
 }
--- src/views/pager.js
+++ src/views/pager.js
@@ -2,21 +2,58 @@ import m from "mithril"
 import Arxiv from "../models/arxiv"
 import Config from "../models/config"
 
+function genPages(page){
+  let array = []
+  let start = Math.max(1, page - 3)
+  for(let i = start; i < start + 7; ++i){
+    array.push(i)
+  }
+  return array
+}
+
 var Pager = {
   view(){
     let category = Arxiv.categories.join('+')
     let start = Arxiv.start
     let prev = Math.max(0, start - Arxiv.resultsNum)
     let next = start + Arxiv.resultsNum
-    let page = start / Arxiv.resultsNum + 1
-    return <nav>
-      { page > 1 &&
-        <a href={`#!/${category}/${prev}`} onclick={() => Arxiv.fetch(prev)}>Prev</a>
+    let currentPage = start / Arxiv.resultsNum + 1
+    return <nav class="tc">
+      <ul class="dib ma2 pa0 f5 center">
+      { currentPage > 1 &&
+        <li class="di">
+          <a href={`#!/${category}/${prev}`} onclick={() => Arxiv.fetch(prev)}
+             class="no-underline blue">
+             <i class="fa fa-angle-double-left"></i>
+          </a>
+        </li>
       }
-      <span>Page {page}</span>
-      <a href={`#!/${category}/${next}`} onclick={() => Arxiv.fetch(next)}>Next</a>
-      <input type="checkbox" checked={Config.summaryShow}
-                             onchange={m.withAttr("checked", Config.setSummaryShow)}/>
+      {
+        genPages(currentPage).map(page =>
+          <li class="di mh2">
+            {
+              page === currentPage ?
+                <span class="b">{page}</span>
+              :
+                <a href={`#!/${category}/${(page - 1) * Arxiv.resultsNum}`}
+                   onclick={() => Arxiv.fetch((page - 1) * Arxiv.resultsNum)}
+                   class="no-underline blue">
+                   {page}
+                </a>
+            }
+          </li>
+        )
+      }
+      { <li class="di">
+          <a href={`#!/${category}/${next}`} onclick={() => Arxiv.fetch(next)}
+             class="no-underline blue">
+             <i class="fa fa-angle-double-right"></i>
+          </a>
+        </li>
+      }
+      </ul>
+      { /* <input type="checkbox" checked={Config.summaryShow}
+                             onchange={m.withAttr("checked", Config.setSummaryShow)}/>*/}
     </nav>
   }
 }
