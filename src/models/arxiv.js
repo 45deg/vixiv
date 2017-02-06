4b5b2864a2269d46d376890258d58ed7370ad9ec
45deg <noreply>
2017-02-06 16:42:59 +0900

    Added totalNum

--- src/models/arxiv.js
+++ src/models/arxiv.js
@@ -12,6 +12,7 @@ var Arxiv = {
   // configuration
   articles : [],
   resultsNum : 15,
+  totalNum: 0,
   start: 0,
   categories: [],
   waiting: false,
@@ -33,7 +34,7 @@ var Arxiv = {
       let entries = [...xml.querySelectorAll('entry')];
       Arxiv.articles = entries.map(entry => ({
         title: getText(entry, 'title'),
-        summary : getText(entry, 'summary').replace(/\r?\n/g, ''),
+        summary : getText(entry, 'summary').replace(/\r?\n/g, ' '),
         published: getText(entry, 'published'),
         id: getText(entry, 'id'),
         author: getTexts(entry, 'author name'),
@@ -41,6 +42,7 @@ var Arxiv = {
         category: [...entry.querySelectorAll('category')]
                     .map(cat => cat.getAttribute('term'))
       }))
+      Arxiv.totalNum = xml.querySelector('opensearch:totalResults').innerHTML
       Arxiv.waiting = false
       Arxiv.start   = start
     })
